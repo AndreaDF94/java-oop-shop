2d62e2f372e967296a4827214f4f8153337e8132
Andrea De Filippi <defilippiandre@libero.it>
2025-07-15 11:10:21 +0200

    Aggiungo il costruttore alla classe Prodotto e assegno i parametri agli attributi della classe

--- App.java/src/prodotto.java
+++ App.java/src/prodotto.java
@@ -11,3 +11,17 @@ public class Prodotto {
     private String descrizione;      // Descrizione testuale
     private double prezzo;           // Prezzo base senza IVA
     private double iva;              // Percentuale IVA da applicare
+
+    // Costruttore che inizializza il prodotto con nome, descrizione, prezzo e iva
+    public Prodotto(String nome, String descrizione, double prezzo, double iva) {
+        // Genero un codice casuale tra 1000 e 9999
+        Random rand = new Random();
+        this.codice = rand.nextInt(9000) + 1000;
+
+        // Assegno i parametri agli attributi della classe
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
