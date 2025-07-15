c06d4ea5e788669173e05ad5669be4817d0a7926
Andrea De Filippi <defilippiandre@libero.it>
2025-07-15 11:19:54 +0200

    Aggiungo la classe App con metodo main per testare la classe Prodotoot

--- App.java/src/App.java
+++ App.java/src/App.java
@@ -1,1 +1,13 @@
+/*
+ * Classe principale App.java
+ * Contiene il metodo main che serve per avviare l'applicazione Java
+ */
+public class App {
+    public static void main(String[] args) {
+        // Creo un nuovo prodotto con nome, descrizione, prezzo e IVA
+        Prodotto p = new Prodotto("Mouse", "Mouse ottico wireless", 25.0, 22.0);
 
+        // Stampo tutti i dettagli del prodotto a schermo
+        p.stampaDettagli();
+    }
+}
